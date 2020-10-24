6d0328664e02d40e5ea7fd831e11fecd6745c6ae
culiu <culiuliu@gmail.com>
2020-10-24 19:33:25 +0800

    no thing

--- 20200926.py
+++ 20200926.py
@@ -1,6 +1,5 @@
-from queue import Queue
+from queue import Queue, LifoQueue
 import sys
-
 class BinaryTreeNode:
     def __init__(self, data, l=None, r=None):
         self.data = data
@@ -72,6 +71,68 @@ class BinaryTree:
             return is_balanced_helper(node.left) and is_balanced_helper(node.right)
 
         return is_balanced_helper(self.root)
+
+    def is_same(self, tree):
+        def is_same_helper(p, q):
+            if p is None:
+                if q is None:
+                    return True
+                else:
+                    return False
+            elif q is None:
+                return False
+            return p.data == q.data and is_same_helper(p.left, p.right) and is_same_helper(q.left, q.right)
+
+        return is_same_helper(self.root, tree.root)
+
+    def is_same_2(self, the_other_tree):
+        stack = LifoQueue()
+        stack.push = stack.put
+        stack.pop = stack.get
+        p, q = self.root, the_other_tree.root
+        stack.push(p)
+        stack.push(q)
+        while not stack.empty():
+            p = stack.pop()
+            q = stack.pop()
+            if p is None and q is None:
+                continue
+            if p is None or q is None:
+                return False
+            if p.data != q.data:
+                return False
+            stack.push(p.left)
+            stack.push(p.right)
+            stack.push(q.left)
+            stack.push(q.right)
+        return True
+
+    def flatten(self):
+        # pass
+        def flatten_helper(node):
+            if node is None:
+                return
+            flatten_helper(node.left)
+            flatten_helper(node.right)
+
+        flatten_helper(self.root)
+
+    def flatten_2(self):
+        node = self.root
+        if node is None:
+            return
+        stack = LifoQueue()
+        stack.put(node)
+        while not stack.empty():
+            p = stack.get()
+            if p.right:
+                stack.put(p.right)
+            if p.left:
+                stack.put(p.left)
+            p.left = None
+            if not stack.empty():
+                p.right = stack.get()
+                stack.put(p.right)
 
     def is_symmetric(self):
         def is_symmetric_helper(left, right):
